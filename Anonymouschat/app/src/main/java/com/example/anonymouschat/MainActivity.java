76b5f4ae41e1735c0eec8e5126ef9bae5f70b25d
Matvey Shadrin <matthew95011@mail.ru>
2019-06-25 01:04:14 +0500

    Измения

--- Anonymouschat/app/src/main/java/com/example/anonymouschat/MainActivity.java
+++ Anonymouschat/app/src/main/java/com/example/anonymouschat/MainActivity.java
@@ -20,10 +20,85 @@ public class MainActivity extends AppCompatActivity {
 
     private static String IP_ADDR = "46.146.137.184";
     private static final int port = 8189;
-
+    TextView textView;
+    TextView name;
+    TextView message;
+    TextView ip;
+    ScrollView scrollView;
+    private static TCPConnection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView10);
+        ip = findViewById(R.id.editText23);
+        name = findViewById(R.id.editText18);
+        message = findViewById(R.id.editText21);
+        scrollView = findViewById(R.id.my_scrole_view);
+        ip.setText(IP_ADDR);
+        new ConnectTask().execute();
+    }
+    public void reconnect(View view){
+        connection.disconnect();
+        IP_ADDR = ip.getText().toString();
+        new ConnectTask().execute();
+
+    }
+
+    public synchronized void sendMessage(View view){
+        String msg = message.getText().toString();
+        if(msg.equals("")){
+            return;
+        }
+        message.setText(null);
+        String usr = name.getText().toString();
+        connection.sendString(usr + ": " +msg);
+    }
+
+    private void printMessage(String msg){
+        textView.append(msg + "\n");
+        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+    }
+
+    public class ConnectTask extends AsyncTask<Void, String, TCPConnection> implements TCPConnectionListener {
+
+        @SuppressLint("WrongThread")
+        @Override
+        protected TCPConnection doInBackground(Void... voids) {
+            try {
+                connection = new TCPConnection(this, IP_ADDR, port);
+            } catch (IOException e) {
+                onProgressUpdate("Connection exception: " + e);
+            }
+            connection.run();
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            printMessage(values[0]);
+        }
+
+        @Override
+        public void onConnectionReady(TCPConnection tcpConnection) {
+            publishProgress("Connection ready...");
+        }
+
+        @Override
+        public void onReceiveString(String value) {
+            publishProgress(value);
+        }
+
+        @Override
+        public void onDisconnect(TCPConnection tcpConnection) {
+            publishProgress("Connection close...");
+        }
+
+        @Override
+        public void onException(TCPConnection tcpConnection, Exception e) {
+            publishProgress("Connection exception: " + e);
+        }
     }
 }
